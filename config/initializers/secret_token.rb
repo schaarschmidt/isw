0c23da0c168e02c9ca664cb2d140973c051846a9
Michael Schaarschmidt <michael.schaarschmidt@itz.uni-halle.de>
2013-07-14 22:35:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InternationalesSaaleschwimmen::Application.config.secret_key_base = '5f59ecfdcf01d898b431f4ce3f65a781aaa82d2489dbc7f7b4d587aae3846cf3596499a67c2cf9518084a483e67aa4cc578c50e85a221f8cef01111265948c40'
